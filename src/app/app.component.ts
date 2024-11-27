aa2560335801322540e16604d688513fd5930641
Jose Favian Reyes <156625483+saeechy@users.noreply.github.com>
2024-11-27 11:59:04 +0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Comp1Component } from './comp1/comp1.component';
-import { Comp2Component } from './comp2/comp2.component';
-import { Comp3Component } from './comp3/comp3.component';
-import { Comp4Component } from './comp4/comp4.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    Comp1Component,
-    Comp2Component,
-    Comp3Component,
-    Comp4Component,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'prelim_lab_1';
+  title = 'prelim_lab_2';
+
+  name: string = 'Jose Favian Pascual Reyes';
+  birthday = 'April, 26, 2004';
+  address = '53 BRGY Aguinaldo, Santa Rose, Nueva Ecija';
+  education = 'Holy Angel University';
+  nationality = 'Filipino';
+  work = 'Business';
+  training = 'None';
+  skillList: string[] = [
+    'Frontend',
+    'Backend',
+    'Fullstack',
+    'Communication',
+    'Analyzing',
+  ];
 }
